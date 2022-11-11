b70bb7ad00cc9310e24d282eca58c25089f19bf2
samiterefe <samuelterefe2@gmail.com>
2022-11-11 16:29:09 +0300

    creating blogs module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BlogsModule } from './blogs/blogs.module';
 
 @Module({
-  imports: [],
+  imports: [BlogsModule],
   controllers: [],
   providers: [],
 })
